bf7fbf9487307807ebfbaf2081c614840e811fbe
= <=>
2022-09-28 17:01:40 -0300

    feat: create create default router and auth router

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,14 @@ import express  from "express";
 import cors from 'cors'
 import "express-async-errors";
 import {errorHandlerMiddleware} from '../src/medleweres/handleErrorsMiddleware'
-
-import { notFoundError } from "./utils/errorUtils";
+import router from "./routes/indexRoutes";
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.use(router)
 
-app.get("/", (req, res) =>{
-
-    throw notFoundError("deu ruin")
-   
-    return res.send("ta lá")
-})
 
 app.use(errorHandlerMiddleware);
 export default app
