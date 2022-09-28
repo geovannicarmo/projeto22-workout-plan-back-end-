27e84adeba5aa156b0d1781db1553c179ffc45eb
= <=>
2022-09-28 14:02:43 -0300

    feat: add validate schema middleware

---
+++ src/medleweres/schemasMidlewares.ts
@@ -0,0 +1,20 @@
+import {Request, Response, NextFunction} from 'express'
+import {ObjectSchema} from 'joi'
+
+
+export default function schemasMidlewares(schema: ObjectSchema){
+
+    return(
+        req: Request,
+        res: Response,
+        next: NextFunction
+    )=>{
+        const {error} = schema.validate( req.body, {abortEarly: false})
+        if(error){
+            const validateError: Array<string> = []
+            error.details.map((error: any)=>{validateError.push(error.message)})
+            return res.send(validateError)
+        }
+        next()
+    }
+}
